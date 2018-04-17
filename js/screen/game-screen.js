93a2299d944cbc9bea14c4e58cae77dcc65eda87
Julia Dolgun <siannn@yandex.ru>
2018-04-17 22:59:42 +0300

    implemented mvp

--- js/screen/game-screen.js
+++ js/screen/game-screen.js
@@ -4,13 +4,15 @@ import Game1View from '../game-1-view.js';
 import Game2View from '../game-2-view.js';
 import Game3View from '../game-3-view.js';
 import Timer from '../timer.js';
-import questions from '../questions.js';
 
 const MAX_TIME = 30;
 
 export default class GameScreen {
+  constructor(model) {
+    this._model = model;
+  }
   addAnswer(correct, time) {
-    this.answers.push({
+    this._model.answers.push({
       correct,
       time
     });
@@ -18,32 +20,29 @@ export default class GameScreen {
 
   changeLocation(questionNumber, correct) {
     if (!correct) {
-      if (this.lives === 0) {
+      if (this._model.lives === 0) {
         questionNumber = ``;
       } else {
-        this.lives--;
+        this._model.lives--;
       }
     }
 
-    if (questions[questionNumber]) {
-      this.question = questionNumber;
+    if (this._model.questions[questionNumber]) {
+      this._model.question = questionNumber;
       this.initNewLocation();
     } else {
-      Application.showStats(this.answers, this.lives);
+      Application.showStats(this._model.answers, this._model.lives);
     }
   }
 
-  init() {
-    this.question = `question1`;
-    this.answers = [];
-    this.lives = 3;
-    this.view = new Game1View(questions[this.question], this.answers, this.lives);
+  startGame() {
+    this.view = new Game1View(this._model.questions[this._model.question], this._model.answers, this._model.lives);
     this.initNewLocation();
   }
 
   initNewLocation() {
     let NeededClass;
-    switch (questions[this.question].location) {
+    switch (this._model.questions[this._model.question].location) {
       case `game-1`:
         NeededClass = Game1View;
         break;
@@ -54,13 +53,13 @@ export default class GameScreen {
         NeededClass = Game3View;
         break;
     }
-    this.view = new NeededClass(questions[this.question], this.answers, this.lives);
+    this.view = new NeededClass(this._model.questions[this._model.question], this._model.answers, this._model.lives);
     showScreen(this.view.element);
     const timer = new Timer(MAX_TIME);
     timer.onTimeEnds = () => {
       const currentIsCorrect = false;
       this.addAnswer(currentIsCorrect, MAX_TIME);
-      this.changeLocation(questions[this.question].next, currentIsCorrect);
+      this.changeLocation(this._model.questions[this._model.question].next, currentIsCorrect);
       timer.stop();
     };
 
@@ -69,21 +68,21 @@ export default class GameScreen {
 
       // добавляем в массив ответов
       let currentIsCorrect;
-      switch (questions[this.question].location) {
+      switch (this._model.questions[this._model.question].location) {
         case `game-1`:
           currentIsCorrect =
-            (questions[this.question].image1.answer === answer1 && questions[this.question].image2.answer === answer2);
+            (this._model.questions[this._model.question].image1.answer === answer1 && this._model.questions[this._model.question].image2.answer === answer2);
           break;
         case `game-2`:
-          currentIsCorrect = (answer1.value === questions[this.question].image.answer);
+          currentIsCorrect = (answer1.value === this._model.questions[this._model.question].image.answer);
           break;
         case `game-3`:
-          currentIsCorrect = (answer1 === questions[this.question].paintNumber);
+          currentIsCorrect = (answer1 === this._model.questions[this._model.question].paintNumber);
           break;
       }
 
       this.addAnswer(currentIsCorrect, MAX_TIME - timer.time);
-      this.changeLocation(questions[this.question].next, currentIsCorrect);
+      this.changeLocation(this._model.questions[this._model.question].next, currentIsCorrect);
     };
 
     this.view.onBackButtonClick = function () {
